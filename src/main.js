67cc8f2991e23053bb13830543a31fc4162fc0d6
Artem <artmyseo@yandex.ru>
2021-11-23 15:16:53 +0300

    добавление router-view

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+import router from './router/index'
 
 // Импортировать файлы CSS Bootstrap и Bootstrap Vue (порядок важен)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-// Сделайте BootstrapVue доступным для всего проекта
 Vue.use(BootstrapVue)
-// При желании установите плагин компонентов иконок BootstrapVue
 Vue.use(IconsPlugin)
+Vue.use(VueRouter)
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
